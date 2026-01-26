90c93c83c1dd56a087743450317fa65bdb8adf23
CRaph0705 <raphael.cochrane94@gmail.com>
2026-01-26 16:56:51 +0100

    subject main added

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,27 +1,41 @@
 #include "main.hpp"
-
+/* 
 int main()
 {
-    int a = 2;
-    int b = 3;
+	int a = 2;
+	int b = 3;
 
 	std::cout << "a = " << a << std::endl;
 	std::cout << "b = " << b << std::endl;
 	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
 	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
 	std::cout << "swap(a, b) -> ";
-    ::swap(a, b);
+	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 
 
-    std::string s1 = "test";
-    std::string s2 = "plop";
+	std::string s1 = "test";
+	std::string s2 = "plop";
 	std::cout << "s1 = \"" << s1 << "\"" << std::endl;
 	std::cout << "s2 = \"" << s2 << "\"" << std::endl;
 	std::cout << "swap(s1, s2) -> ";
-    ::swap(s1, s2);
-    std::cout << "s1 = \"" << s1 << "\", s2 = \"" << s2 << "\"" << std::endl;
+	::swap(s1, s2);
+	std::cout << "s1 = \"" << s1 << "\", s2 = \"" << s2 << "\"" << std::endl;
 
-    return 0;
+	return 0;
+} */
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 }
-
